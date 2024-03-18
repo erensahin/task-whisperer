1d9d84fe3dce57d921b85905662a6c22564c6599
Eren Sahin <sahineren.09@gmail.com>
2024-03-18 23:26:05 +0300

    use .env files to load env variables and use omegaconf to resolve config file

--- task_whisperer/__init__.py
+++ task_whisperer/__init__.py
@@ -1,9 +1,12 @@
 import os
-import yaml
+
+from dotenv import load_dotenv
+from omegaconf import OmegaConf
+
+
+load_dotenv()
 
 PROJECT_ROOT = os.path.dirname(__file__)
 CONFIG_PATH = os.path.join(PROJECT_ROOT, "config.yml")
 
-with open(CONFIG_PATH, "r") as f:
-    CONFIG = yaml.safe_load(f)
-    DEFAULTS = CONFIG.get("defaults", {})
+CONFIG = OmegaConf.to_container(OmegaConf.load(CONFIG_PATH), resolve=True)
