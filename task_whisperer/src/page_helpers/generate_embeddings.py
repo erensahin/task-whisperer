997e97b0ead2d7b35dd8c92b5627ebab4bde3d2b
Eren Sahin <sahineren.09@gmail.com>
2024-03-24 00:17:34 +0300

    reorganize generate_embeddings page: use classes to reduce parameter passing between functions

--- task_whisperer/src/page_helpers/generate_embeddings.py
+++ task_whisperer/src/page_helpers/generate_embeddings.py
@@ -16,87 +16,88 @@ EMBEDDINGS_ROOT_PATH = os.path.join(
 FAISS_ROOT_PATH = os.path.join(EMBEDDINGS_ROOT_PATH, "faiss")
 
 
-def get_issues_meta(its_kind: str):
-    issue_service = IssueService(its_config={}, its_kind=its_kind)
-    issues_meta = issue_service.load_metadata()
-    issues_meta = {row["project"]: row for row in issues_meta}
-    return issues_meta
+class GenerateEmbeddingsService:
+    def __init__(self, llm_kind: str, its_kind: str, llm_config: Dict[str, Any]):
+        self.llm_kind = llm_kind
+        self.its_kind = its_kind
+        self.llm_config = llm_config
 
+    def get_issues_meta(self) -> Dict[str, Dict[str, Any]]:
+        issue_service = IssueService(its_config={}, its_kind=self.its_kind)
+        issues_meta = issue_service.load_metadata()
+        issues_meta = {row["project"]: row for row in issues_meta}
+        return issues_meta
 
-def create_embeddings(
-    llm_kind: str, its_kind: str, project: str, llm_config: Dict[str, Any]
-) -> str:
-    os.makedirs(FAISS_ROOT_PATH, exist_ok=True)
-    issues_meta = get_issues_meta(its_kind)
-    project_meta = issues_meta[project]
-    processed_issues_df = pd.read_csv(project_meta["processed_issues_path"])
+    def create_embeddings(self, project: str) -> str:
+        os.makedirs(FAISS_ROOT_PATH, exist_ok=True)
+        issues_meta = self.get_issues_meta()
+        project_meta = issues_meta[project]
+        processed_issues_df = pd.read_csv(project_meta["processed_issues_path"])
 
-    embedding_client = embedding_factory.get(llm_kind)(
-        api_key=llm_config["api_key"],
-        faiss_index_root_path=FAISS_ROOT_PATH,
-        embedding_model=llm_config["embedding_model"],
-    )
+        embedding_client = embedding_factory.get(self.llm_kind)(
+            api_key=self.llm_config["api_key"],
+            faiss_index_root_path=FAISS_ROOT_PATH,
+            embedding_model=self.llm_config["embedding_model"],
+        )
 
-    embedding_path, embedding_cb = embedding_client.generate_embeddings(
-        project, processed_issues_df
-    )
-    # TODO: use a logger here
-    print(embedding_cb)
-    return embedding_path
+        embedding_path, embedding_cb = embedding_client.generate_embeddings(
+            project, processed_issues_df
+        )
 
+        # TODO: use a logger here
+        print(embedding_cb)
 
-def load_metadata(llm_kind: str) -> Optional[List[Dict]]:
-    try:
-        meta_path = os.path.join(FAISS_ROOT_PATH, llm_kind, "_meta.yml")
-        meta = _read_metadata(meta_path)
+        return embedding_path
 
-        meta_prepared = []
-        for project, project_meta in meta.items():
-            created_at = datetime.utcfromtimestamp(project_meta["_created_at"])
-            updated_at = datetime.utcfromtimestamp(project_meta["_updated_at"])
-            meta_prepared.append(
-                {
-                    "project": project,
-                    "created_at": created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                    "updated_at": updated_at.strftime("%Y-%m-%d %H:%M:%S"),
-                    "embedding_path": project_meta["embedding_path"],
-                }
-            )
+    def load_metadata(self) -> Optional[List[Dict]]:
+        try:
+            meta_path = os.path.join(FAISS_ROOT_PATH, self.llm_kind, "_meta.yml")
+            meta = self._read_metadata(meta_path)
 
-        return meta_prepared
-    except:
-        return None
+            meta_prepared = []
+            for project, project_meta in meta.items():
+                created_at = datetime.utcfromtimestamp(project_meta["_created_at"])
+                updated_at = datetime.utcfromtimestamp(project_meta["_updated_at"])
+                meta_prepared.append(
+                    {
+                        "project": project,
+                        "created_at": created_at.strftime("%Y-%m-%d %H:%M:%S"),
+                        "updated_at": updated_at.strftime("%Y-%m-%d %H:%M:%S"),
+                        "embedding_path": project_meta["embedding_path"],
+                    }
+                )
 
+            return meta_prepared
+        except:
+            return None
 
-def save_embeddings_paths(
-    llm_kind: str, project_embedding_paths: Dict[str, Any]
-) -> None:
-    meta_path = os.path.join(FAISS_ROOT_PATH, llm_kind, "_meta.yml")
-    meta = _read_metadata(meta_path)
+    def save_embeddings_paths(self, project_embedding_paths: Dict[str, Any]) -> None:
+        meta_path = os.path.join(FAISS_ROOT_PATH, self.llm_kind, "_meta.yml")
+        meta = self._read_metadata(meta_path)
 
-    for project, embedding_path in project_embedding_paths.items():
-        now_ts = datetime.now().timestamp()
+        for project, embedding_path in project_embedding_paths.items():
+            now_ts = datetime.now().timestamp()
 
-        new_info = {"_updated_at": now_ts, "embedding_path": embedding_path}
+            new_info = {"_updated_at": now_ts, "embedding_path": embedding_path}
 
-        if project in meta:
-            meta[project] = {**meta[project], **new_info}
-        else:
-            meta[project] = {"_created_at": now_ts, **new_info}
+            if project in meta:
+                meta[project] = {**meta[project], **new_info}
+            else:
+                meta[project] = {"_created_at": now_ts, **new_info}
 
-    _write_metadata(meta, meta_path)
+        self._write_metadata(meta, meta_path)
 
+    @staticmethod
+    def _read_metadata(meta_path: str) -> Dict[str, Any]:
+        try:
+            with open(meta_path) as f:
+                meta = yaml.safe_load(f) or {}
+        except:
+            meta = {}
 
-def _read_metadata(meta_path: str):
-    try:
-        with open(meta_path) as f:
-            meta = yaml.safe_load(f) or {}
-    except:
-        meta = {}
+        return meta
 
-    return meta
-
-
-def _write_metadata(meta: Dict, meta_path: str):
-    with open(meta_path, "w") as f:
-        yaml.dump(meta, f)
+    @staticmethod
+    def _write_metadata(meta: Dict, meta_path: str):
+        with open(meta_path, "w") as f:
+            yaml.dump(meta, f)
--- task_whisperer/streamlit_app/pages/2_generate_embeddings.py
+++ task_whisperer/streamlit_app/pages/2_generate_embeddings.py
@@ -1,44 +1,65 @@
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 import streamlit as st
 
 from task_whisperer.src.page_helpers.generate_embeddings import (
-    create_embeddings,
-    save_embeddings_paths,
-    load_metadata,
+    GenerateEmbeddingsService,
 )
 from task_whisperer.src.steamlit_helpers.sidebar import render_sidebar
 
 
-def generate_embeddings(
-    llm_kind: str, its_kind: str, llm_config: Dict[str, Any], projects: List[str]
-):
-    embeddings_paths = {}
-    for project in projects:
-        embeddings_path = create_embeddings(llm_kind, its_kind, project, llm_config)
-        embeddings_paths[project] = embeddings_path
+class GenerateEmbeddingsRenderer:
+    def __init__(self, its_config: Dict[str, Any], llm_config: Dict[str, Any]):
+        self.its_config = its_config
+        self.llm_config = llm_config
+        self.its_kind = self.its_config["selected_its"]
+        self.llm_kind = self.llm_config["selected_llm"]
+        self.projects = self.its_config["projects"].split(",")
+        self.projects = [p for p in self.projects]
+        self.generate_embeddings_service = GenerateEmbeddingsService(
+            self.llm_kind, self.its_kind, self.llm_config
+        )
 
-    save_embeddings_paths(llm_kind, embeddings_paths)
+    def generate_embeddings(self) -> None:
+        embeddings_paths = {}
+        for project in self.projects:
+            embeddings_path = self.generate_embeddings_service.create_embeddings(
+                project
+            )
+            embeddings_paths[project] = embeddings_path
 
+        self.generate_embeddings_service.save_embeddings_paths(embeddings_paths)
 
-def render_embedding_generation_layout(
-    llm_kind: str, its_kind: str, llm_config: Dict[str, Any], projects: List[str]
-):
-    st.markdown("### Click Generate Embeddings button to create embeddings")
-    project_names = ",".join(projects)
-    st.markdown(f"**Projects: {project_names}**")
-    st.markdown("A separate embedding index will be created for each project.")
-    submitted = st.button("Generate Embeddings 🚀", type="primary")
-    if submitted:
-        if not llm_config["api_key"]:
-            st.warning(f"Please enter your {llm_kind} API key to continue.", icon="⚠️")
-        else:
-            with st.spinner(
-                f"Generating {llm_kind} embeddings for {project_names}. "
-                "Please wait..."
-            ):
-                generate_embeddings(llm_kind, its_kind, llm_config, projects)
-                st.success("Embeddings generated successfully! 🎉")
+    def render_embedding_generation_layout(self) -> None:
+        st.markdown("### Click Generate Embeddings button to create embeddings")
+        project_names = ",".join(self.projects)
+        st.markdown(f"**Projects: {project_names}**")
+        st.markdown("A separate embedding index will be created for each project.")
+        submitted = st.button("Generate Embeddings 🚀", type="primary")
+        if submitted:
+            if not self.llm_config["api_key"]:
+                st.warning(
+                    f"Please enter your {self.llm_kind} API key to continue.", icon="⚠️"
+                )
+            else:
+                with st.spinner(
+                    f"Generating {self.llm_kind} embeddings for {project_names}. "
+                    "Please wait..."
+                ):
+                    self.generate_embeddings()
+                    st.success("Embeddings generated successfully! 🎉")
+
+    def render_page_container(self) -> None:
+        with st.container(border=True):
+            col1, col2 = st.columns(2)
+
+            with col1:
+                renderer.render_embedding_generation_layout()
+
+            with col2:
+                st.markdown("### Project - Faiss Indices on Internal Datastore")
+                jira_task_metadata = self.generate_embeddings_service.load_metadata()
+                st.table(jira_task_metadata)
 
 
 if __name__ == "__main__":
@@ -46,19 +67,8 @@ if __name__ == "__main__":
     sidebar_config = render_sidebar()
     st.title("Generate Embeddings From Task Summaries & Descriptions")
 
-    its_kind = sidebar_config["its_config"]["selected_its"]
-    llm_config = sidebar_config["llm_config"]
-    llm_kind = llm_config["selected_llm"]
-    projects = sidebar_config["its_config"]["projects"].split(",")
-    projects = [p for p in projects]
+    renderer = GenerateEmbeddingsRenderer(
+        sidebar_config["its_config"], sidebar_config["llm_config"]
+    )
 
-    with st.container(border=True):
-        col1, col2 = st.columns(2)
-
-        with col1:
-            render_embedding_generation_layout(llm_kind, its_kind, llm_config, projects)
-
-        with col2:
-            st.markdown("### Project - Faiss Indices on Internal Datastore")
-            jira_task_metadata = load_metadata(llm_kind)
-            st.table(jira_task_metadata)
+    renderer.render_page_container()
